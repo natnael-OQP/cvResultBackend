790128912c09352ab132374a871bcac70300f33a
natnael-OQP <makiya112545@gmail.com>
2022-03-21 16:23:57 +0300

    connect to mongodb

---
+++ db.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const dbConnect = async () => {
+  await mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(() => console.log("Connected to MongoDB"));
+};
+
+module.exports = dbConnect;
